a5edaf217cf47c526db2489ec93b5c3f6f347565
Sandi Rohman N <127264824+SandiR1P@users.noreply.github.com>
2023-10-14 15:33:03 +0700

    Add files via upload

---
+++ pertemuan 1/pertemuan 1 no 1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("NIM 2341065 \n");
+	printf("Nama Saya Sandi Rohman N \n");
+	printf("degnan Jurusan Teknik Informatika");
+	
+	return 0;
+}
---
+++ pertemuan 1/pertemuan 1 no 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	float panjang, lebar, luas;
+    panjang = 10.0;
+    lebar = 7.0;
+
+    luas = panjang * lebar;
+
+    printf("Panjang: %.2f cm\n", panjang);
+    printf("Lebar: %.2f cm\n", lebar);
+    printf("Luas Persegi Panjang: %.2f cm\n", luas);
+
+    return 0;
+}
